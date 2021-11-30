36686e0092d4565fd146cdcc36f784a3606f3213
Allan Zhang <allanzhang7@gmail.com>
2021-11-30 18:06:33 -0330

    Add deserializing of summary.txt

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,51 @@ use std::{fs, path::Path};
 
 use serde::Deserialize;
 
-#[derive(Deserialize)]
-struct Summary {
-    pass: bool,
-    filename: String,
+#[derive(Debug, Deserialize)]
+enum Flag {
+	#[serde(rename = "PASS")]
+	Pass,
+	#[serde(rename = "FAIL")]
+	Fail,
+	#[serde(rename = "WARN")]
+	Warn,
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+struct Summary {
+	flag: Flag,
+	test: String,
+	filename: String,
+}
 fn main() {
-    let data_dir = Path::new("data");
+	let data_dir = Path::new("data");
 
-    let paths = fs::read_dir(data_dir).unwrap();
+	let paths = fs::read_dir(data_dir).unwrap();
 
-    // Check all paths in the data directory.
-    for path in paths {
-        println!("{}", path.unwrap().path().display());
-    }
-    println!("Hello, world!");
+	// Check all paths in the data directory.
+	for path in paths {
+		let folder_path = path.unwrap().path();
+
+		if !folder_path.is_dir() {
+			continue;
+		}
+
+		for file_path in fs::read_dir(folder_path).unwrap() {
+			let direntry = file_path.unwrap();
+			match direntry.file_name().to_str().unwrap() {
+				"summary.txt" => {
+					let file = fs::File::open(direntry.path()).unwrap();
+					let mut rdr = csv::ReaderBuilder::new()
+						.delimiter(b'\t')
+						.has_headers(false)
+						.from_reader(file);
+					for result in rdr.deserialize::<Summary>() {
+						println!("{:?}", result.unwrap());
+					}
+				}
+				_ => (),
+			}
+		}
+	}
 }
