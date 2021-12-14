47b3c543dd096a5dfac278e5ff7723b5dd1a8716
allan2 <allanzhang7@gmail.com>
2021-12-14 16:49:05 -0330

    Add tabs

--- toggle.ts
+++ toggle.ts
@@ -1,39 +1,76 @@
+// mutable state
 let showFailOnly = false;
-let totalCount = document.querySelectorAll('figure').length;
-let passCount = document.querySelectorAll('.pass').length;
-let numResults = totalCount;
-let resultsCountEl = document.getElementById("results-count");
+const getCounts = (divId: string): [number, number] => {
+	const d = document.getElementById(divId) as HTMLDivElement;
+	return [d.querySelectorAll('figure').length, d.querySelectorAll('.pass').length];
+}
+let [totalCount, passCount] = getCounts("before-trim-content");
 
-const showingMsg = (count: number): string => {
-	let s = `Showing ${count} result`;
-	if (count != 1) {
+let numResults = totalCount;
+
+let resultsCountEl = document.getElementById("results-count") as HTMLSpanElement;
+
+const showingMsg = (n: number): string => {
+	let s = `Showing ${n} result`;
+	if (n != 1) {
 		s += "s";
 	}
 	return s
 }
 
-if (resultsCountEl !== null) {
-	resultsCountEl.innerHTML = showingMsg(numResults);
-}
+resultsCountEl.innerHTML = showingMsg(numResults);
 
 const toggleShowFailOnly = () => {
 	showFailOnly = !showFailOnly;
-	let x = document.getElementsByClassName("pass") as HTMLCollectionOf<HTMLElement>;
+	const x = document.getElementsByClassName("pass") as HTMLCollectionOf<HTMLElement>;
 	for (let i = 0; i < x.length; i++) {
-		if (x[i].style.display == "none") {
-			x[i].style.display = "block";
-		} else {
-			x[i].style.display = "none";
-		}
+		toggleVisible(x[i]);
 	}
 
-	if (resultsCountEl !== null) {
-		let numResults: number;
-		if (!showFailOnly) {
-			numResults = totalCount;
-		} else {
-			numResults = passCount;
-		}
-		resultsCountEl.innerHTML = showingMsg(numResults);
+	if (!showFailOnly) {
+		numResults = totalCount;
+	} else {
+		numResults = passCount;
 	}
+	resultsCountEl.innerHTML = showingMsg(numResults);
 }
+
+const toggleVisible = (el: HTMLElement) => (el.style.display == "none") ? el.style.display = "block" : el.style.display = "none";
+
+let checkBox = document.getElementById("check");
+checkBox?.addEventListener("change", toggleShowFailOnly);
+
+const openTab = (e: Event) => {
+	// Hide all tab content elements.
+	let tabContentEls = document.getElementsByClassName("tab-content") as HTMLCollectionOf<HTMLDivElement>;
+	for (let i = 0; i < tabContentEls.length; i++) {
+		tabContentEls[i].style.display = "none";
+	}
+	// Remove the active class from the tab buttons.
+	let tabBtns = document.getElementsByClassName("tab-btn") as HTMLCollectionOf<HTMLButtonElement>;
+	for (let i = 0; i < tabBtns.length; i++) {
+		tabBtns[i].classList.remove("active");
+		tabBtns[i].disabled = false;
+	}
+
+	let currEl = e.target as HTMLButtonElement;
+	let contentId = "before-trim-content";
+	if (currEl.id === "after-trim-btn") {
+		contentId = "after-trim-content";
+	}
+	// Update the counts.
+	[totalCount, passCount] = getCounts(contentId);
+	// Show the tab's content.
+	let content = document.getElementById(contentId) as HTMLDivElement;
+	content.style.display = "block";
+	// Make th tab button active.
+	currEl.classList.add("active");
+	currEl.disabled = true;
+}
+
+const tabBtns = document.querySelectorAll(".tab-btn");
+for (let i = 0; i < tabBtns.length; i++) {
+	tabBtns[i].addEventListener("click", openTab);
+}
+
+
