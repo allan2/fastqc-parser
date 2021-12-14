2be8db7b8a19066e4e9aaa84aa94527f8b94d19f
allan2 <allanzhang7@gmail.com>
2021-12-14 00:59:19 -0330

    Remove outdated log

--- toggle.ts
+++ toggle.ts
@@ -1,4 +1,23 @@
+let showFailOnly = false;
+let totalCount = document.querySelectorAll('figure').length;
+let passCount = document.querySelectorAll('.pass').length;
+let numResults = totalCount;
+let resultsCountEl = document.getElementById("results-count");
+
+const showingMsg = (count: number): string => {
+	let s = `Showing ${count} result`;
+	if (count != 1) {
+		s += "s";
+	}
+	return s
+}
+
+if (resultsCountEl !== null) {
+	resultsCountEl.innerHTML = showingMsg(numResults);
+}
+
 const toggleShowFailOnly = () => {
+	showFailOnly = !showFailOnly;
 	let x = document.getElementsByClassName("pass") as HTMLCollectionOf<HTMLElement>;
 	for (let i = 0; i < x.length; i++) {
 		if (x[i].style.display == "none") {
@@ -6,5 +25,15 @@ const toggleShowFailOnly = () => {
 		} else {
 			x[i].style.display = "none";
 		}
+	}
+
+	if (resultsCountEl !== null) {
+		let numResults: number;
+		if (!showFailOnly) {
+			numResults = totalCount;
+		} else {
+			numResults = passCount;
+		}
+		resultsCountEl.innerHTML = showingMsg(numResults);
 	}
 }
